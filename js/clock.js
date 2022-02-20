285921bb8fc71e53fa85482cc214d75427acec96
oiseo1201 <oiseo1201@naver.com>
2022-02-21 05:35:10 +0900

    CHALLENGE

---
+++ js/clock.js
@@ -0,0 +1,16 @@
+const clock = document.querySelector('h2#clock');
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  console.log(hours);
+  if (hours < 12) {
+    clock.innerText = `${hours}:${minutes} AM`;
+  } else {
+    clock.innerText = `${hours}:${minutes} PM`;
+  }
+}
+getClock();
+setInterval(getClock, 1000);
